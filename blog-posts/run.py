423c3cdab31beaafeac48d1d2da3a7ea8a0d3c4f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-19 22:27:15 -0500

    Activando la ejecución del proyecto de modo debug

--- blog-posts/run.py
+++ blog-posts/run.py
@@ -2,4 +2,5 @@ from blogr import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+    # estableciendo que la aplicación se ejecute en modo debug
+    app.run(debug = True)
