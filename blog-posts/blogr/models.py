4d1f34b894aa9fb4a61c3d3e5397cb325ea86e9a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-20 22:27:16 -0500

    Creando el modelo para Post con sus respectivos atributos, constructor y proceso de impresión en cosola

--- blog-posts/blogr/models.py
+++ blog-posts/blogr/models.py
@@ -1,4 +1,5 @@
 from blogr import db
+from datetime import datetime
 
 # Creando el modelo para User
 class User(db.Model):
@@ -18,3 +19,25 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User: '{self.username}'"
+ 
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    url = db.Column(db.String(100), unique = True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    info = db.Column(db.Text)
+    content = db.Column(db.Text)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+
+    def __init__(self, author, url, title, info, content) -> None :
+        self.author = author
+        self.url = url
+        self.title = title
+        self.info = info
+        self.content = content
+
+    # se imprime por consola solo el titulo del post creado
+    def __repr__(self) -> str:
+        return f'Post: {self.title}'
