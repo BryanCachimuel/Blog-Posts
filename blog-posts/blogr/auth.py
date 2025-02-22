34c50287583ea1a0f22e91541118ca820627e819
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-21 21:21:14 -0500

    Creando el script dentro de la función register para registrar y validar que un usuario ya existente no se pueda registrar varias veces

--- blog-posts/blogr/auth.py
+++ blog-posts/blogr/auth.py
@@ -1,9 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect, flash, session, g
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .models import User
+from blogr import db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/register')
+@bp.route('/register', methods = ('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # objeto para registrar en la base de datos
+        user = User(username, email, generate_password_hash(password))
+
+        # validación de datos
+        error = None
+
+        # Comparando nombre de usuario con los existentes
+        user_email = User.query.filter_by(email = email).first()
+        if user_email == None:
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+        else:
+            error = f'El correo {email} ya está registrado'
+
+        flash(error)
     return render_template('auth/register.html')
 
 @bp.route('/login')
