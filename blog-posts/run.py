18ea7b4e5101ecb900afec282978033f7b8400b6
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-21 22:22:34 -0500

    Se estable mediante la configuración del proyecto que se ejecute el mismo mediante el proceso debug y en el template de index se han realizado actualizaciones en las urls de la vista

--- blog-posts/run.py
+++ blog-posts/run.py
@@ -3,4 +3,4 @@ from blogr import create_app
 if __name__ == '__main__':
     app = create_app()
     # estableciendo que la aplicación se ejecute en modo debug
-    app.run(debug = True)
+    app.run()
