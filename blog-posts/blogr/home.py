f136cae8d107b2e341a5e1a8bbbb4f82e75e2f24
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-21 22:19:54 -0500

    Redirigiendo a las vistas respectivas de blog e index

--- blog-posts/blogr/home.py
+++ blog-posts/blogr/home.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('home', __name__)
 
 @bp.route('/')
 def index():
-    return 'Página de Inicio'
+    return render_template('index.html')
 
 @bp.route('/blog')
 def blog():
-    return 'Página de Blog'
+    return render_template('blog.html')
