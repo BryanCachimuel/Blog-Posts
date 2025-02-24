59757d4931e69324ba9deabf1136b5ca2402e544
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-23 21:41:31 -0500

    Creando el procedimiento para guardar y editar la fotografía de perfil de un usuario

--- blog-posts/blogr/auth.py
+++ blog-posts/blogr/auth.py
@@ -71,6 +71,7 @@ def logout():
     session.clear()
     return redirect(url_for('home.index'))
 
+
 # Decorador para afirmar que es necesario iniciar sesión para acceder a determinadas vistas
 import functools
 
@@ -82,10 +83,23 @@ def login_required(view):
         return view(**kwargs)
     return wrapped_view
 
+# Editar Perfil
+from werkzeug.utils import secure_filename
+
+def  get_photo(id):
+    user = User.query.get_or_404(id)
+    photo = None
+
+    if photo != None:
+        photo = user.photo
+
+    return photo
+
 @bp.route('/profile/<int:id>', methods=('GET', 'POST'))
 @login_required
 def profile(id):
     user = User.query.get_or_404(id)
+    photo = get_photo(id)
 
     if request.method == 'POST':
         user.username = request.form.get('username')
@@ -97,6 +111,12 @@ def profile(id):
         elif len(password) > 0 and len(password) < 6:
             error = 'La contraseña deve terner mas 5 caracteres'
 
+        
+        if request.files['photo']:
+            photo = request.files['photo']
+            photo.save(f'blogr/static/media/{secure_filename(photo.filename)}')
+            user.photo = f'media/{secure_filename(photo.filename)}'
+        
         if error is not None:
             flash(error)
         else:
@@ -105,4 +125,4 @@ def profile(id):
         
         flash(error)
 
-    return render_template('auth/profile.html', user = user)
+    return render_template('auth/profile.html', user = user, photo = photo)
