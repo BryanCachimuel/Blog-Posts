9233868fec826e94c9c1716c4d3776ac50631253
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-24 21:27:26 -0500

    Adecuando la vista de blog para obtener los datos tanto del usuario como del blog que va a leer

--- blog-posts/blogr/home.py
+++ blog-posts/blogr/home.py
@@ -19,4 +19,4 @@ def index():
 def blog(url):
     # buscar un registro en la base de datos mediante url
     post = Post.query.filter_by(url = url).first()
-    return render_template('blog.html', post = post)
+    return render_template('blog.html', post = post, get_user = get_user)
