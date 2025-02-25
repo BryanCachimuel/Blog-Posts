27a0e24801c370d19fb5a3876c0539eca8d4fa23
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-24 21:44:54 -0500

    Implementando un función para buscar los posts por cualquier tipo de dato que se ingrese en el input del buscador del proyecto

--- blog-posts/blogr/home.py
+++ blog-posts/blogr/home.py
@@ -9,10 +9,25 @@ def get_user(id):
     user = User.query.get_or_404(id)
     return user
 
-@bp.route('/')
+# función para dar funcionalidad al buscador
+def search_posts(query):
+    # ilike -> recibe los textos que se le van a enviar por el buscador y va a encontrar todas las coincidencias y por ende nos devuelve el dato buscado
+    posts = Post.query.filter(Post.title.ilike(f'%{query}%')).all()
+    return posts
+
+
+@bp.route('/', methods=['GET','POST'])
 def index():
     # recuperando todos los blogs
     posts = Post.query.all()
+
+    # proceso para el funcionamiento del buscador de posts
+    if request.method == 'POST':
+        query = request.form.get('search')
+        posts = search_posts(query)
+        value = 'hidden'
+        return render_template('index.html', posts = posts, get_user = get_user, value =  value)
+
     return render_template('index.html', posts = posts, get_user = get_user)
 
 @bp.route('/blog/<url>')
